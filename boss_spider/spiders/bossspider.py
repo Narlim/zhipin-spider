136cfee0002248f7ea7181d283cc12209a81e68b
marlin <wangweimingooo@gmail.com>
2020-04-22 21:27:26 +0800

    first commit

---
+++ boss_spider/spiders/bossspider.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_splash import SplashRequest
+
+
+class BossspiderSpider(scrapy.Spider):
+    name = 'bossspider'
+    
+    def __init__(self, location=None, position=None, **kwargs):
+        super().__init__(**kwargs)
+        self.location = location
+        self.position = position
+        self.lua_script = f'''
+        function main(splash, args)
+            splash.private_mode_disable = false
+            splash:on_request(function(request)
+            request:set_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0")')
+            end)
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(5))
+            input_box = assert(splash:select(".ipt-search"))
+            input_box:focus()
+            input_box:send_text("{self.position}")
+            assert(splash:wait(0.5))
+            input_box:send_keys("<Enter>")
+            assert(splash:wait(10))
+            return splash:html()
+        end
+        '''
+
+    def start_requests(self):
+        yield SplashRequest(url=f"https://www.zhipin.com/{self.location}/",
+                            callback=self.parse,
+                            endpoint="execute",
+                            args={'lua_source': self.lua_script})
+    
+
+    def parse(self, response):
+        for job in response.xpath("//div[@class='job-primary']"):
+            yield {
+                'job_name': job.xpath(".//span[@class='job-name']/a/text()").get(),
+                'job_area': job.xpath(".//span[@class='job-area']/text()").get(),
+                'company': job.xpath(".//div[@class='company-text']/h3/a/text()").get(),
+                'salary': job.xpath(".//span[@class='red']/text()").get(),
+                'required': job.xpath(".//div[@class='job-limit clearfix']/p/text()").get().repace('\n', ' ')
+            }
